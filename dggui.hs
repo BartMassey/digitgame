9b49ff19b0c47bc6dbfc2b9e00f3d4cf5af8fec0
Bart Massey <bart@cs.pdx.edu>
2007-09-30 03:25:31 -0700

    added logic for toggle buttons vs go button
    darcs-hash:20070930102531-1f50a-bdef898d3d9102810a4b492b54fbcc83b97ef707.gz

--- dggui.hs
+++ dggui.hs
@@ -36,14 +36,27 @@ main = do
          (d1, d2) <- roll_dice
          imageSetFromPixbuf die_1 (die_images !! (d1 - 1))
          imageSetFromPixbuf die_2 (die_images !! (d2 - 1))
-  dice_roller
+         return (d1, d2)
+  (d1, d2) <- dice_roller
   die_hsep <- hSeparatorNew
   button_box <- hBoxNew True 0
-  digit_buttons <- mapM (toggleButtonNewWithLabel . show) [1..9]
+  digit_buttons <- mapM (\i -> toggleButtonNewWithLabel (show i)) [1..9]
+  go_button <- buttonNewWithLabel "Go"
+  let update_status i = do
+         clicked <- mapM (\i -> do
+                            a <- toggleButtonGetActive
+                                 (digit_buttons !! (i - 1))
+                            if a then return i else return 0) [1..9]
+         if (sum clicked) == d1 + d2
+            then onClicked go_button (do (d1, d2) <- dice_roller ; return ())
+            else onClicked go_button (return ())
+         return ()
+  let set_up_button (b, i) = do
+         onToggled b (update_status i)
+  mapM_ set_up_button (zip digit_buttons [1..9])
   mapM_ (\b -> boxPackStart button_box b PackGrow 0) digit_buttons
   ok_vsep <- vSeparatorNew
   boxPackStart button_box ok_vsep PackGrow 0
-  go_button <- buttonNewWithLabel "Go"
   boxPackStart button_box go_button PackGrow 0
   boxPackStart box die_box PackGrow 0
   boxPackStart box die_hsep PackGrow 0
