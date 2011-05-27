360c11161ef46909850e21ffcd480ef9570ba304
Bart Massey <bart@cs.pdx.edu>
2011-05-27 00:33:52 -0700

    updated some stuff

--- digitgame.hs
+++ digitgame.hs
@@ -1,20 +1,15 @@
--- Copyright © 2011 Bart Massey
--- [This program is licensed under the "MIT License"]
+-- Copyright © 2005-2007 Bart Massey <bart@cs.pdx.edu> 
+--   and Jamey Sharp <jamey@cs.pdx.edu>
+-- [This program is licensed under the "MIT License".]
 -- Please see the file COPYING in the source
 -- distribution of this software for license terms.
 
-{-
-#
-# digitgame
-# Copyright 2005-2007 Bart Massey <bart@cs.pdx.edu> and
-# Jamey Sharp <jamey@cs.pdx.edu>
-# ALL RIGHTS RESERVED
-# See the end of this file for licensing information
-#
-# Game with dice and digits (a wooden game owned
-# by the Andreas Junghanns family)
-#
--}
+-- digitgame
+
+-- Game with dice and digits (a wooden game owned by the
+-- Andreas Junghanns family). This is a game most commonly
+-- known as "Shut The Box".
+--   https://secure.wikimedia.org/wikipedia/en/wiki/Shut_the_Box
 
 module Main where
 
@@ -296,33 +291,3 @@ main =
                      else rollDice
         play roller threshold `catch` (putStrLn . ioeGetErrorString)
         putStrLn "Thanks for playing!"
-
-{-
-  Permission is hereby granted, free of charge, to any person
-  obtaining a copy of this software and associated
-  documentation files (the "Software"), to deal in the
-  Software without restriction, including without limitation
-  the rights to use, copy, modify, merge, publish, distribute,
-  sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
-  
-  The above copyright notice and this permission notice shall
-  be included in all copies or substantial portions of the
-  Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-  KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-  OTHER DEALINGS IN THE SOFTWARE.
-  
-  Except as contained in this notice, the names of the authors
-  or their institutions shall not be used in advertising or
-  otherwise to promote the sale, use or other dealings in this
-  Software without prior written authorization from the
-  authors.
--}
